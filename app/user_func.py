1c899a109fb17fa0c7ff664991abaec5166b09bb
bibi643 <77496652+bibi643@users.noreply.github.com>
2023-11-02 23:52:44 +0100

    Add files via upload
    This python file create new users and allow them to login to have access to all endpoints excetp the one to update the databases.

---
+++ app/user_func.py
@@ -0,0 +1,87 @@
+
+"""This python file is to deal with users given them JWT"""
+
+
+from fastapi import Request, HTTPException, Body, Depends
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from pydantic import BaseModel
+from fastapi import FastAPI
+import time
+import jwt
+
+
+JWT_SECRET = "secret"
+JWT_ALGORITHM = "HS256"
+
+users = []
+
+
+class UserSchema(BaseModel):
+    username: str
+    password: str
+
+
+def check_user(data: UserSchema):
+    for user in users:
+        if user.username == data.username and user.password == data.password:
+            return True
+    return False
+
+
+def token_response(token: str):
+    return {"access_token": token}
+
+
+def sign_jwt(user_id: str):
+    payload = {"user_id": user_id, "expires": time.time() + 600}
+    token = jwt.encode(payload, JWT_SECRET, algorithm=JWT_ALGORITHM)
+
+    return token_response(token)
+
+
+def decode_jwt(token: str):
+    try:
+        decoded_token = jwt.decode(
+            token, JWT_SECRET, algorithms=[JWT_ALGORITHM]
+        )
+        return (
+            decoded_token if decoded_token["expires"] >= time.time() else None
+        )
+    except Exception:
+        return {}
+    
+
+
+class JWTBearer(HTTPBearer):
+    def __init__(self, auto_error: bool = True):
+        super(JWTBearer, self).__init__(auto_error=auto_error)
+
+    async def __call__(self, request: Request):
+        credentials: HTTPAuthorizationCredentials = await super(
+            JWTBearer, self
+        ).__call__(request)
+        if credentials:
+            if not credentials.scheme == "Bearer":
+                raise HTTPException(
+                    status_code=403, detail="Invalid authentication scheme."
+                )
+            if not self.verify_jwt(credentials.credentials):
+                raise HTTPException(
+                    status_code=403, detail="Invalid token or expired token."
+                )
+            return credentials.credentials
+        else:
+            raise HTTPException(
+                status_code=403, detail="Invalid authorization code."
+            )
+
+    def verify_jwt(self, jwtoken: str):
+        isTokenValid: bool = False
+
+        try:
+            payload = decode_jwt(jwtoken)
+        except Exception:
+            payload = None
+        if payload:
+            isTokenValid = True
+        return isTokenValid
