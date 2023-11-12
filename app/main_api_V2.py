fd3eced2d18a5ae20e0fa061cda31ea6e2b62bab
bibi643 <77496652+bibi643@users.noreply.github.com>
2023-11-12 19:26:13 +0100

    Add files via upload
    Each route has an error message if something goes wrong. Put methods are in standby for now (commented) because not working and ruining the code.

--- app/main_api_V2.py
+++ app/main_api_V2.py
@@ -24,7 +24,7 @@ from fastapi import Depends, FastAPI, HTTPException, status, Body, Request
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from passlib.context import CryptContext
 import jwt
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, date
 import time
 from admin_func import get_current_admin
 from user_func import UserSchema, check_user, token_response, sign_jwt, decode_jwt, JWTBearer
@@ -46,9 +46,14 @@ player2_base=player2_base.rename(columns={'p2_Name': 'Player_name',
 player_base=pd.concat([player1_base,player2_base],axis=0)
 player_base=player_base.reset_index().drop(columns='index')
 player_base_updated=player_base.groupby(by='Player_name').last()
-player_base_updated_df=player_base_updated.sort_values(by=['Player_name']).reset_index().reset_index()
-player_base_updated_df['index']=player_base_updated_df['index']+1
-player_base_updated_df=player_base_updated_df.rename(columns={'index':'Player_ID'})
+player_base_updated_df=player_base_updated.reset_index()
+player_base_updated_df['Player_name']=player_base_updated_df['Player_name'].str.strip()
+player_base_updated_df=player_base_updated_df.sort_values(by=['Player_name'])
+
+#player_base_updated_df=player_base_updated.sort_values(by=['Player_name']).reset_index().reset_index()
+#player_base_updated_df['index']=player_base_updated_df['index']+1
+
+#player_base_updated_df=player_base_updated_df.rename(columns={'index':'Player_ID'})
 player_base_updated_db= player_base_updated_df.to_dict('records')
 
 
@@ -58,6 +63,7 @@ tournois_df=df_preprocessed[['location','date','tournament','series','surface']]
 tournois_df=tournois_df.groupby(by=['tournament']).first()
 tournois_df=tournois_df.reset_index().reset_index().rename(columns={'index':'Tournois_ID'})
 tournois_df['Tournois_ID']=tournois_df['Tournois_ID']+1
+tournois_df['tournament']=tournois_df['tournament'].str.upper()
 
 #ici les characteristiques du tournoi sont dummifiees. Sans doute utile lorsque le user choisira deux joueru maia aussi le nom du tournoi.
 tournois_df_dum=pd.get_dummies(tournois_df,columns=['series','surface'],prefix='',prefix_sep='')
@@ -67,19 +73,25 @@ tournois_db=tournois_df_dum.to_dict('records')
 
 
 #CLASS TO UPDATE THE API WITH NEW DATAS (ROUTE PUT)
-class Player(BaseModel):
-    name: str
-    rank: int
-    elo: Optional[float] = 1500.0
+#class Player(BaseModel):
+   # name: str
+   # rank: int
+  #  elo: Optional[float] = 1500.0
 
 
 class Tournament(BaseModel):
     name: str
     location: str
-    date: datetime
+    date: date
     series: str
     surface: str
 
+class Player(BaseModel):
+    #Player_id: max(player_base_updated_db, key=lambda u: u.get('Player_ID'))['Player_ID']+1
+    Player_name: str
+    Player_rank: int
+    Player_ELO: Optional[float] = 1500.0
+  
 
 
 #CLASS FOR SECURITY
@@ -113,7 +125,7 @@ api=FastAPI(
     description='API propre faite sur FastAPI',
     openapi_tags=[
         {'name':'home',
-         'description':'Default fucntion'
+         'description':'Default function'
         },
         {'name':'Players',
         'description':'Functions that are used to deal with players'},
@@ -138,40 +150,51 @@ def get_home():
     return {'Greetings':'Bienvenue'}
 
 
-@api.get('/player_from_list',tags=['Players'],dependencies=[Depends(JWTBearer())])
-def get_player_from_list(joueur1_choisi: str = Query("Joueur ATP", enum={joueur['Player_name']for joueur in player_base_updated_db}),joueur2_choisi: str = Query("Joueur ATP", enum={joueur['Player_name']for joueur in player_base_updated_db})):
-    """Return the datas of the selected players"""
-    p1=list(filter(lambda x:x.get('Player_name')==joueur1_choisi,player_base_updated_db))
-    p2=list(filter(lambda x:x.get('Player_name')==joueur2_choisi,player_base_updated_db))
-    return p1,p2
 
+
+@api.get('/player_from_list',tags=['Players'],dependencies=[Depends(JWTBearer())])
+def get_player_from_list(joueur1_choisi:str ,joueur2_choisi:str ):
+    """Return the datas of the selected players"""
+    if joueur1_choisi not in list(player_base_updated_df['Player_name']):
+        raise ValueError ('Player 1 is not in the database. Please enter a valid player such as Federer R.')
+    
+    elif joueur2_choisi not in list(player_base_updated_df['Player_name']):
+        raise ValueError('Player 2 not in database. Please enter a valid player such as Nadal R.')
+    
+    else:
+        p1=list(filter(lambda x:x.get('Player_name')==joueur1_choisi,player_base_updated_db))
+        p2=list(filter(lambda x:x.get('Player_name')==joueur2_choisi,player_base_updated_db))
+        return p1,p2
+    
 
 
 
 
 @api.get('/tournament_from_list',tags=['Tournaments'],dependencies=[Depends(JWTBearer())])
-def get_tournament_from_list(tournois_choisi: str = Query("TOURNAMENT_LIST_ATP", enum={tournament['tournament']for tournament in tournois_db})):
+def get_tournament_from_list(tournois_choisi: str | None='FRENCH OPEN'):
     """Return the datas of the selected tournament"""
-    t=list(filter(lambda y:y.get('tournament')==tournois_choisi,tournois_db))
-    return t
-
-
+    if tournois_choisi.upper() not in list(tournois_df['tournament']):
+        raise ValueError('Tournament not in database')
+    else:
+        t=list(filter(lambda y:y.get('tournament')==tournois_choisi.upper(),tournois_db))
+        return t
+   
 
 @api.get('/new_game',tags=['Players'],dependencies=[Depends(JWTBearer())])
-def new_game_list(date:str=Query('YYYY-MM-DD'), player1: str=Query("Joueur ATP", enum={joueur['Player_name']for joueur in player_base_updated_db}), player2:str=Query("Joueur ATP", enum={joueur['Player_name']for joueur in player_base_updated_db})):
+def new_game_list(date:str=Query('2018-03-03'), player1: str| None='Jarry N.', player2:str| None='Zeballos H.'):
     """Return the game between 2 players on a specific date"""
     #player1=player1.upper()
     #player2=player2.upper()
-    try:
-         specific_game=df_preprocessed[((df_preprocessed['p1_Name']==player1) | (df_preprocessed['p2_Name']==player1))&((df_preprocessed['p1_Name']==player2) | (df_preprocessed['p2_Name']==player2))&(df_preprocessed['date']==date)]
-         specific_game=specific_game.to_dict('records')
-         return specific_game
+    
+    specific_game=df_preprocessed[((df_preprocessed['p1_Name']==player1) | (df_preprocessed['p2_Name']==player1))&((df_preprocessed['p1_Name']==player2) | (df_preprocessed['p2_Name']==player2))&(df_preprocessed['date']==date)]
+    specific_game=specific_game.to_dict('records')
+    if specific_game == []:
+        raise ValueError('Wrong date or players.')
+    else:
+        return specific_game
         
     
 
-
-    except IndexError:
-        {}
 
 
 
@@ -183,62 +206,64 @@ def new_game_pred(date:str=Query('YYYY-MM-DD'), player1: str=Query("Joueur ATP",
     #player2=player2.upper()
     specific_game=df_preprocessed[((df_preprocessed['p1_Name']==player1) | (df_preprocessed['p2_Name']==player1))&((df_preprocessed['p1_Name']==player2) | (df_preprocessed['p2_Name']==player2))&(df_preprocessed['date']==date)]
     specific_game=specific_game.to_dict('records')
-    nd=pd.DataFrame.from_records(specific_game)
-    x_new=feature_eng_predict(nd)
-    model = load_model('./atp_logistic_model.pkl')
-    if model.predict(x_new.head(5))==1 :
-
-        winner = '1'
-        nom = nd.p1_Name
-        cote = nd.p1_b365
-
+    if specific_game == []:
+        raise ValueError('Wrong date or players.')
     else:
-        
-        winner = '2'
-        nom = nd.p2_Name
-        cote = nd.p2_b365
+        nd=pd.DataFrame.from_records(specific_game)
+        x_new=feature_eng_predict(nd)
+        model = load_model('./atp_logistic_model.pkl')
+        if model.predict(x_new.head(5))==1 :
 
-    return {'Winner': winner,
-           'Nom': nom,
-           'Cote': cote}
+            winner = '1'
+            nom = nd.p1_Name
+            cote = nd.p1_b365
 
+        else:
+            
+            winner = '2'
+            nom = nd.p2_Name
+            cote = nd.p2_b365
 
-    
+        return {'Winner': winner,
+            'Nom': nom,
+            'Cote': cote}
 
-@api.put('/players',tags=['Players'])
-def put_player(player: Player,username: str = Depends(get_current_admin)):
-    """Add a new player in the player database"""
-    new_id = max(player_base_updated_db, key=lambda u: u.get('Player_ID'))['Player_ID']
-    new_player= {
-        'Player_ID': new_id +1,
-        'Player_name': player.name,
-        'Player_rank': player.rank,
-        'Player_ELO': player.elo
-    }
-    player_base_updated_db.append(new_player)
-    return new_player #player base_updated not being updated
 
     
 
-@api.put('/tournois',tags=['Tournaments'])
-def put_tournoi(new_tournois: Tournament,username: str = Depends(get_current_admin)):
-    new_id_tournois = max(tournois_db, key=lambda u: u.get('Tournois_ID'))['Tournois_ID']
-    new_tournois= {
-        'Tournois_ID': new_id_tournois +1,
-        'tournament': new_tournois.name,
-        'location': new_tournois.location,
-        'date': new_tournois.date,
-        'series':new_tournois.series,
-        'surface':new_tournois.surface
-    }
-    nt=pd.DataFrame([new_tournois])
-    nt['date']=pd.to_datetime(nt['date'])
-    new_tournois_dum=pd.get_dummies(nt,columns=['series','surface'],prefix='',prefix_sep='')
-    nt_tournois=tournois_df_dum.merge(new_tournois_dum,how='outer')
-    tournois_df_dum=nt_tournois.fillna(0.0)
-    tournois_db=tournois_df_dum.to_dict('records')
 
-    return new_tournois_dum.to_dict('records')
+
+
+
+
+#@api.post('/player_from_list',tags=['Players'])
+#def post_player(new_player: Player,username: str = Depends(get_current_admin)):
+ #   """Add a new player in the player database"""
+    
+ #   player_base_updated_db.append(new_player)
+   
+ #   return new_player #player base_updated not being updated
+    
+
+#@api.put('/tournois',tags=['Tournaments'])
+#def put_tournoi(new_tournois: Tournament,username: str = Depends(get_current_admin)):
+  #  new_id_tournois = max(tournois_db, key=lambda u: u.get('Tournois_ID'))['Tournois_ID']
+ #  new_tournois= {
+ #       'Tournois_ID': new_id_tournois +1,
+ #       'tournament': new_tournois.name,
+ #       'location': new_tournois.location,
+ #       'date': new_tournois.date,
+ #       'series':new_tournois.series,
+  #      'surface':new_tournois.surface
+  #  }
+ #   nt=pd.DataFrame([new_tournois])
+ #   nt['date']=pd.to_datetime(nt['date'])
+ #   new_tournois_dum=pd.get_dummies(nt,columns=['series','surface'],prefix='',prefix_sep='')
+ #   nt_tournois=tournois_df_dum.merge(new_tournois_dum,how='outer')
+ #   tournois_df_dum=nt_tournois.fillna(0.0)
+ #   tournois_db=tournois_df_dum.to_dict('records')
+
+ #   return new_tournois_dum.to_dict('records')
 
 
 
