26701ea507286377cb4243bb772e9362980e646a
Ubuntu <ubuntu@ip-172-31-34-51.eu-west-1.compute.internal>
2023-12-04 22:22:38 +0000

    add class model and remove others

--- src/main.py
+++ src/main.py
@@ -1,51 +1,56 @@
 # Package
 
 from data_loader import load_data
-from utile import path_db
+from utile import path_db, log_separator
 from preprocessing import preprocess_data
 from feature_eng import feature_eng
-from modele import logistic_regression_model
-from modele_training import train_logistic_regression_model
-from modele_predict import accuracy_and_confusion_matrix
-from model_save import save_model_pickle, save_model_joblib
+
+from model import LogisticRegressionHandler
+
+
+
+
 
 # 01 Import data
 print('Lancement du code 01: data_loader.py')
 df = load_data(path_db)
 print('taille du dataframe:', df.shape)
+log_separator()
 
 # 02 Preprocessing data
 print('lancement du code 2: preprocessing.py')
 test = preprocess_data(df)
 print('output preprocess:', test.shape)
 print('colonne names:', test.columns)
+log_separator()
 
 # 03 feature enginering
 print('Lancement du code 3: feature_eng.py')
 X_train, X_test, y_train, y_test = feature_eng(test)
 print('shape', X_train.shape, 'shape_test:', X_test.shape)
+log_separator()
 
-# 04 modele
-print('Lancement du code 4: modele.py')
-logistic_model = logistic_regression_model()
-print("modele : ", logistic_model)
+# 04 modele training
+print('Lancement du code 4: modele_training.py')
+# Créer une instance de la classe LogisticRegressionHandler
+logistic_regression_handler = LogisticRegressionHandler()
+logistic_regression_handler.train_model(X_train, y_train)
+log_separator()
 
-# 05 modele training
-print('Lancement du code 5: modele_training.py')
-y_pred = train_logistic_regression_model(
-    X_train, X_test, y_train, y_test, logistic_model)
-print('shape pred: ', y_pred.shape)
+# 05 prediction
+y_pred = logistic_regression_handler.predict(X_test)
+log_separator()
 
-# 06 modele predict
-print('Lancement du code 6: modele_predict.py')
-accuracy, confusion, classification_rep = accuracy_and_confusion_matrix(
-    y_test, y_pred)
+# 06 Evaluate model
+print('Lancement du code 6: evaluate')
+accuracy, confusion, classification_rep = logistic_regression_handler.evaluate(y_test, y_pred)
 print(f"Précision du modèle : {accuracy}")
 print("Matrice de confusion :")
 print(confusion)
 print("Rapport de classification :")
 print(classification_rep)
+log_separator()
 
 # 07 model save
-save_model_pickle(logistic_model, 'atp_logistic_model.pkl')
-save_model_joblib(logistic_model, 'atp_logistic_model.joblib')
+logistic_regression_handler.save_model_pickle('atp_logistic_model.pkl')
+logistic_regression_handler.save_model_pickle('atp_logistic_model.joblib')
--- src/main_predict.py
+++ src/main_predict.py
@@ -1,46 +1,45 @@
 # Package
 
 from data_loader import load_data
-from utile import path_db
+from utile import path_db, log_separator
 from preprocessing import preprocess_data
 from feature_eng import feature_eng, feature_eng_predict
-from modele_predict import accuracy_and_confusion_matrix
-from model_save import load_model
+from model import LogisticRegressionHandler
+
+
+
 
 
 # 01 Import data
 print('Lancement du code 01: data_loader.py')
 df = load_data(path_db)\
     .sample(1)
-    
-
+log_separator()
 
 # 02 Preprocessing data
 print('lancement du code 2: preprocessing.py')
 test = preprocess_data(df)
 print('output preprocess:', test.shape)
+print('colonne names:', test.columns)
+log_separator()
 
-#Select the player with API input
-
-#test.query('p1_Name=={} and p2_Name={} and date ={}'.format())
-
-
-
-
-# 03 Feature_eng_predict
-print('lancement du code 3: Feature_eng_predict.py')
+# 03 feature enginering
+print('Lancement du code 3: feature_eng.py')
 x_new = feature_eng_predict(test)
-print('Shape X_new', x_new.shape)
+print('shape', x_new.shape)
+log_separator()
 
 
 # 04 Load model
+logistic_regression_handler = LogisticRegressionHandler()
 
-model = load_model('./atp_logistic_model.pkl')
+
+model = logistic_regression_handler.load_model('./atp_logistic_model.pkl')
 
 
 # 05 predict new data
 
-if model.predict(x_new.head(5))==1 :
+if model.predict(x_new.head(1))==1 :
 
     winner = '1'
     nom = test.p1_Name
