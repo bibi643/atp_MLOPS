22c3827ca86243425b2cacfa4171cc615a1896e2
bibi643 <77496652+bibi643@users.noreply.github.com>
2023-12-14 14:28:57 +0100

    Delete Mlops_projectDocker/src directory
    nettoasyeg

--- Mlops_projectDocker/src/api/data_loader.py
+++
@@ -1,9 +0,0 @@
-#Package
-import pandas as pd
-
-
-def load_data(path :str):
-
-    df = pd.read_csv(path, sep=',')
-    
-    return(df)
--- Mlops_projectDocker/src/model/modele.py
+++
@@ -1,6 +0,0 @@
-from sklearn.linear_model import LogisticRegression
-
-def logistic_regression_model():
-    model = LogisticRegression()
-
-    return model
