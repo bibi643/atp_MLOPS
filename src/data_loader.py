52de7c1a216aab48e844b88c3f50cfefd21069c4
Ubuntu <ubuntu@ip-172-31-34-51.eu-west-1.compute.internal>
2023-12-15 20:51:13 +0000

    add class datahandler

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,9 +1,23 @@
-#Package
 import pandas as pd
 
+class DataHandler():
+    def __init__(self):
+        self.path_db = "https://github.com/bibi643/atp_MLOPS/raw/main/atp_data.csv"
+        self.path_new_data = "http://tennis-data.co.uk/2023/2023.xlsx"
+        self.path_save = "/home/ubuntu/test.csv"
 
-def load_data(path :str):
+    def load_data(self):
 
-    df = pd.read_csv(path, sep=',')
+        df = pd.read_csv(self.path_db, sep=',')
+        return(df)
+
+    def add_data(self):
+
+        df = pd.read_excel(self.path_new_data)
+        return(df)
     
-    return(df)
+    def save_date(self, df):
+        """
+        Write csv file 
+        """
+        df.to_csv(self.path_save, sep=",")
